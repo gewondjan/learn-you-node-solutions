8a4878451809102634caa6454525a7f690963243
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2018-10-15 13:37:20 -0600

    finished problem 4

--- 4 My First Async IO.js
+++ 4 My First Async IO.js
@@ -1,1 +1,11 @@
 /* eslint-disable */
+
+var fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', function done(err, fileContents) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(fileContents.split('\n').length - 1);
+});
